60a0d3e78256bc8433e03342121c873f0bdeebc8
nathanon11 <38213@virtual.prc.ac.th>
2021-10-05 12:09:26 +0700

    Update Cs01-14.py

--- Cs01-14.py
+++ Cs01-14.py
@@ -2,5 +2,5 @@ thislist = (5,10,15,20,25,50,20)
 for i in range(len(thislist)) :
     print(i)
     if(thislist(i) == "20") :
-             thislist(i) = "200"
+         thislist(i) = "200"
 print(thislist)
